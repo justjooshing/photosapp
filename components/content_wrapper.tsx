164f0508229c67ecb50952a3bd9465b7c7db676a
Josh <joshua.l.mertens@gmail.com>
2024-05-20 15:37:41 +1000

    fix: setminHeight rather than fixed

--- components/content_wrapper.tsx
+++ components/content_wrapper.tsx
@@ -19,12 +19,12 @@ export default ContentWrapper;
 const styles = StyleSheet.create({
   appWrapper: {
     alignItems: "center",
-    height: "100%",
+    minHeight: "100%",
     backgroundColor: "#ccc",
   },
   contentWrapper: {
     width: "100%",
-    height: "100%",
+    minHeight: "100%",
     maxWidth: 700,
     backgroundColor: "white",
     paddingHorizontal: Platform.OS === "web" && width > 700 ? 30 : 0,
