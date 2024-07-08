93a0658db51258d0574929b5a9e576cf5d8c01ca
Josh <joshua.l.mertens@gmail.com>
2024-07-08 16:32:43 +1000

    refactor: replace loading skeleton with more lightweight alternative

--- components/skeleton.tsx
+++ components/skeleton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import Animated, {
   useAnimatedStyle,
@@ -6,43 +6,25 @@ import Animated, {
   withRepeat,
   withTiming,
 } from "react-native-reanimated";
-import { Shadow } from "react-native-shadow-2";
 
 import { tokens } from "@/config/tamagui/tokens";
 
 const Skeleton = () => {
-  const [offsetWidth, setOffsetWidth] = useState(180);
-  const offset = useSharedValue(0);
-
-  const animatedStyles = useAnimatedStyle(() => ({
-    transform: [{ translateX: offset.value }],
-  }));
+  const opacity = useSharedValue(1);
 
   useEffect(() => {
-    offset.value = withRepeat(withTiming(offsetWidth, { duration: 1500 }), -1);
-    return () => {
-      offset.value = -150;
-    };
-  }, [offsetWidth]);
+    opacity.value = withRepeat(withTiming(0.3, { duration: 1000 }), -1, true);
+  }, []);
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
 
   return (
-    <View
-      style={styles.box}
-      onLayout={({ nativeEvent }) => {
-        setOffsetWidth(nativeEvent.layout.width);
-      }}
-    >
-      <Animated.View style={[styles.shimmer, animatedStyles]}>
-        <View style={styles.bar}>
-          <Shadow
-            style={styles.shadow}
-            containerStyle={styles.shadow_container}
-            distance={50}
-            sides={{ start: false, bottom: false, top: false, end: true }}
-            startColor={tokens.color.grey1}
-          />
-        </View>
-      </Animated.View>
+    <View style={[styles.size, styles.initialColour]}>
+      <Animated.View
+        style={[styles.size, styles.animatedColour, animatedStyles]}
+      />
     </View>
   );
 };
@@ -50,24 +32,14 @@ const Skeleton = () => {
 export default Skeleton;
 
 const styles = StyleSheet.create({
-  box: {
+  size: {
     width: "100%",
     height: "100%",
-    backgroundColor: tokens.color.grey2,
-    justifyContent: "center",
-    overflow: "hidden",
   },
-  shimmer: {
-    height: "110%",
+  initialColour: {
+    backgroundColor: tokens.color.grey1,
   },
-  bar: {
-    transform: "rotate(5deg)",
-    height: "100%",
-  },
-  shadow_container: {
-    opacity: 0.4,
-  },
-  shadow: {
-    height: "100%",
+  animatedColour: {
+    backgroundColor: tokens.color.grey4,
   },
 });
