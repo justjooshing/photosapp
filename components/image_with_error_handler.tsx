738c55ce280158777ac47e1046b2b9697ce658e1
Josh <joshua.l.mertens@gmail.com>
2024-08-05 17:36:55 +1000

    fix: ensure errored image matches styling of non-errored image

--- components/image_with_error_handler.tsx
+++ components/image_with_error_handler.tsx
@@ -1,6 +1,6 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import React, { useState } from "react";
-import { Image, ImageProps, StyleSheet } from "react-native";
+import { Image, ImageProps, StyleSheet, View } from "react-native";
 
 import { tokens } from "@/config/tamagui/tokens";
 
@@ -19,18 +19,24 @@ const ImageWithError = ({ imageProps, errorProps = { size: 96 } }: Props) => {
       {...imageProps}
     />
   ) : (
-    <MaterialIcons
-      name="image-not-supported"
-      color={tokens.color.red2}
-      size={errorProps.size}
-      style={styles.icon}
-    />
+    <View style={[imageProps.style, styles.error_container]}>
+      <MaterialIcons
+        name="image-not-supported"
+        color={tokens.color.red2}
+        size={errorProps.size}
+        style={styles.icon}
+      />
+    </View>
   );
 };
 
 export default ImageWithError;
 
 const styles = StyleSheet.create({
+  error_container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
   icon: {
     justifyContent: "center",
     alignItems: "center",
