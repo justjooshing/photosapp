cbe9462b7e158cff0d41e2c809557a504db66d3b
Josh <joshua.l.mertens@gmail.com>
2024-08-07 09:48:24 +1000

    feat: trigger deeplinking attempt once

--- hooks/useDeeplink.ts
+++ hooks/useDeeplink.ts
@@ -1,36 +1,40 @@
 import * as Linking from "expo-linking";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const handleDeepLink = ({ url }) => {
+  const data = Linking.parse(url);
+  const appUrlScheme = "picpurge";
+
+  // Only try opening app when not already on app url
+  if (data.scheme !== appUrlScheme) {
+    const urlSearchParams = new URLSearchParams();
+    for (const [key, val] of Object.entries(data.queryParams)) {
+      if (typeof val === "string") urlSearchParams.append(key, val);
+    }
+    const appUrl = `${appUrlScheme}://${urlSearchParams.size ? `?${urlSearchParams.toString()}` : ""}`;
+    Linking.openURL(appUrl);
+  }
+};
 
 const useDeeplink = () => {
+  const [hadDeepLinkPrompt, setHadDeeplinkPrompt] = useState(false);
   useEffect(() => {
-    const handleDeepLink = ({ url }) => {
-      const data = Linking.parse(url);
-      const appUrlScheme = "picpurge";
-
-      // Only try opening app when not already on app url
-      if (data.scheme !== appUrlScheme) {
-        const urlSearchParams = new URLSearchParams();
-        for (const [key, val] of Object.entries(data.queryParams)) {
-          if (typeof val === "string") urlSearchParams.append(key, val);
+    if (!hadDeepLinkPrompt) {
+      setHadDeeplinkPrompt(true);
+      // If url opened directly
+      Linking.getInitialURL().then((url) => {
+        if (url) {
+          handleDeepLink({ url });
         }
-        const appUrl = `${appUrlScheme}://${urlSearchParams.size ? `?${urlSearchParams.toString()}` : ""}`;
-        Linking.openURL(appUrl);
-      }
-    };
+      });
 
-    // If url opened directly
-    Linking.getInitialURL().then((url) => {
-      if (url) {
-        handleDeepLink({ url });
-      }
-    });
-
-    // When url changes
-    const deepLinkingEvent = Linking.addEventListener("url", handleDeepLink);
-    return () => {
-      deepLinkingEvent.remove();
-    };
-  }, []);
+      // When url changes
+      const deepLinkingEvent = Linking.addEventListener("url", handleDeepLink);
+      return () => {
+        deepLinkingEvent.remove();
+      };
+    }
+  }, [hadDeepLinkPrompt]);
 };
 
 export default useDeeplink;
