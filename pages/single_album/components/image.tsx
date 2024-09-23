316d60fe1b296b3481d9d8754c8c44b63251a7ff
Josh <joshua.l.mertens@gmail.com>
2024-09-23 12:06:27 +1000

    refactor: update styles for single_album buttons
    chore: delete now unused component

--- pages/single_album/components/image.tsx
+++ pages/single_album/components/image.tsx
@@ -1,53 +1,115 @@
 import { AntDesign } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
-import { View, Pressable, StyleSheet } from "react-native";
+import { View, Pressable, StyleSheet, Text } from "react-native";
 
-import Button from "./button";
-
+import { useUpdateSingleAlbumImage } from "@/api/images/mutations";
 import { ApiImage, SortOptions } from "@/api/types";
 import ImageTile from "@/components/image_tile";
 import { tokens } from "@/config/tamagui/tokens";
+import { Button } from "@/config/tamagui/variants";
 import { useImageContext } from "@/context/image";
 
-type Props = { image: ApiImage };
+const copy = {
+  markAs: (updatedChoice: string) => `Mark to be ${updatedChoice}`,
+  google: "Delete from Google Photos",
+};
 
+type Props = { image: ApiImage };
 const Image = ({ image }: Props) => {
+  const { mutate: updateImage } = useUpdateSingleAlbumImage(
+    image.sorted_album_id.toString(),
+  );
   const { setTargetImage } = useImageContext();
+
   const handleButtonClick = () => {
     WebBrowser.openBrowserAsync(image.productUrl);
     // Keep track of which image we've just viewed to refetch baseUrl
     setTargetImage(image);
   };
 
+  const { markAsLabel, updated_status, markAsBgColor, markAsColor } = {
+    [SortOptions.KEEP]: {
+      markAsLabel: copy.markAs("deleted"),
+      markAsBgColor: tokens.color.red,
+      markAsColor: tokens.color.white,
+      updated_status: SortOptions.DELETE,
+    },
+    [SortOptions.DELETE]: {
+      markAsLabel: copy.markAs("kept"),
+      markAsBgColor: tokens.color.green,
+      markAsColor: tokens.color.white,
+      updated_status: SortOptions.KEEP,
+    },
+  }[image.sorted_status];
+
+  const handleClick = () => {
+    updateImage({
+      imageId: image.id,
+      sorted_status: updated_status,
+    });
+  };
+
   return (
-    <>
+    <View style={styles.image}>
       <ImageTile baseUrl={image.baseUrl} />
       <View style={styles.button_wrapper}>
-        {Object.values(SortOptions).map((val) => (
-          <Button key={val} image={image} choice={val} />
-        ))}
-        <Pressable onPress={handleButtonClick}>
-          <AntDesign
-            name="google"
-            size={20}
-            color={tokens.color.black}
-            style={styles.button_google}
-          />
+        <Pressable
+          onPress={handleClick}
+          style={[
+            styles.image_sort,
+            {
+              backgroundColor: markAsBgColor,
+            },
+          ]}
+        >
+          <Button.Text
+            style={{
+              color: markAsColor,
+            }}
+          >
+            {markAsLabel}
+          </Button.Text>
+        </Pressable>
+        <Pressable onPress={handleButtonClick} style={styles.image_google}>
+          <AntDesign name="google" size={20} color={tokens.color.black} />
+          <Text>{copy.google}</Text>
         </Pressable>
       </View>
-    </>
+    </View>
   );
 };
 
 export default Image;
 
 const styles = StyleSheet.create({
+  image: {
+    alignItems: "center",
+    paddingVertical: 20,
+    width: "100%",
+  },
   button_wrapper: {
-    width: "60%",
-    flexDirection: "row",
+    maxWidth: "80%",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingTop: 10,
+    paddingTop: tokens.space[1],
+    gap: tokens.space[1],
   },
-  button_google: { userSelect: "none" },
+  image_sort: {
+    width: "100%",
+    textAlign: "center",
+    alignItems: "center",
+    padding: tokens.space[1],
+  },
+  image_google: {
+    userSelect: "none",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: tokens.color.grey2,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: tokens.space[1],
+    gap: tokens.space[1],
+    textAlign: "center",
+  },
 });
